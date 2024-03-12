a09f10b58474338d699f24ec9565ccb48b3b1f9f
seunghee17 <victoria9981@swu.ac.kr>
2024-03-13 00:08:20 +0900

    [design] primary button 구현 완료

--- core/designsystem/src/main/java/com/numberone/daepiro/designsystem/theme/components/buttons/DefaultButton.kt
+++ core/designsystem/src/main/java/com/numberone/daepiro/designsystem/theme/components/buttons/DefaultButton.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
---
+++ core/designsystem/src/main/java/com/numberone/daepiro/designsystem/theme/components/buttons/FilledButton.kt
@@ -0,0 +1,222 @@
+package com.numberone.daepiro.designsystem.theme.components.buttons
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.numberone.daepiro.core.designsystem.R
+import com.numberone.daepiro.designsystem.theme.themes.DaepiroTheme
+import model.PrimaryButtonState
+
+@Composable
+fun FilledButton(filledButtonState: PrimaryButtonState, text:String, onClick: () -> Unit){
+    when(filledButtonState){
+        PrimaryButtonState.ICONDEFAULT -> FilledButton_default(text = text, onClick = onClick, leftIcon = true)
+        PrimaryButtonState.DEFAULT -> FilledButton_default(text = text, onClick = onClick, leftIcon = false)
+        PrimaryButtonState.ICONPRESSED -> FilledButton_pressed(text = text, onClick = onClick, leftIcon = true)
+        PrimaryButtonState.PRESSED -> FilledButton_pressed(text = text, onClick = onClick, leftIcon = false)
+        PrimaryButtonState.ICONDISABLED -> FilledButton_disabled(text = text, onClick = onClick, leftIcon = true)
+        PrimaryButtonState.DISABLED ->FilledButton_disabled(text = text, onClick = onClick, leftIcon = false)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun FilledButton_default(
+    modifier: Modifier = Modifier,
+    text: String,
+    buttonRadius: Dp = 4.dp,
+    onClick: () -> Unit,
+    leftIcon: Boolean,
+){
+    val interactionSource = remember{ MutableInteractionSource() }
+
+    Box(
+        modifier = modifier
+            .background(DaepiroTheme.colors.Primary, shape = RoundedCornerShape(buttonRadius))
+            .fillMaxWidth()
+            .clickable (
+                interactionSource = interactionSource,
+                indication = null,
+            ){
+                onClick()
+            }
+            .padding(vertical = 13.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$text",
+            color = DaepiroTheme.colors.On_Primary,
+            style = DaepiroTheme.typography.button,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        if (leftIcon) {
+            Row(
+                modifier = Modifier.align(Alignment.CenterStart),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    modifier = Modifier.padding(start = 16.dp),
+                    painter = painterResource(id = R.drawable.ic_alarm),
+                    colorFilter = ColorFilter.tint(DaepiroTheme.colors.On_Primary),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+//그림자를 포함시켜야함
+fun FilledButton_pressed(
+    modifier: Modifier = Modifier,
+    text: String,
+    buttonRadius: Dp = 4.dp,
+    onClick: () -> Unit,
+    leftIcon: Boolean,
+){
+    val interactionSource = remember{ MutableInteractionSource() }
+
+    Box(
+        modifier = modifier
+            .background(DaepiroTheme.colors.O_600, shape = RoundedCornerShape(buttonRadius))
+            .fillMaxWidth()
+            .clickable (
+                interactionSource = interactionSource,
+                indication = null,
+            ){
+                onClick()
+            }
+            .padding(vertical = 13.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$text",
+            color = DaepiroTheme.colors.On_Primary,
+            style = DaepiroTheme.typography.button,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        if (leftIcon) {
+            Row(
+                modifier = Modifier.align(Alignment.CenterStart),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    modifier = Modifier.padding(start = 16.dp),
+                    painter = painterResource(id = R.drawable.ic_alarm),
+                    colorFilter = ColorFilter.tint(DaepiroTheme.colors.On_Primary),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+//그림자를 포함시켜야함
+fun FilledButton_disabled(
+    modifier: Modifier = Modifier,
+    text: String,
+    buttonRadius: Dp = 4.dp,
+    onClick: () -> Unit,
+    leftIcon: Boolean,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(
+        modifier = modifier
+            .background(DaepiroTheme.colors.G_50, shape = RoundedCornerShape(buttonRadius))
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+            ) {
+                onClick()
+            }
+            .padding(vertical = 13.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$text",
+            color = DaepiroTheme.colors.G_400,
+            style = DaepiroTheme.typography.button,
+        )
+        if (leftIcon) {
+            Row(
+                modifier = Modifier.align(Alignment.CenterStart),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    modifier = Modifier.padding(start = 16.dp),
+                    painter = painterResource(id = R.drawable.ic_alarm),
+                    colorFilter = ColorFilter.tint(DaepiroTheme.colors.G_400),
+                    contentDescription = null
+                )
+            }
+        }
+
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun FilledButtonDefault_icon_preview() {
+    FilledButton(PrimaryButtonState.ICONDEFAULT, text = "String", onClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FilledButtonDefault_preview() {
+    FilledButton(PrimaryButtonState.DEFAULT, text = "String", onClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FilledButtonPressed_icon_preview() {
+    FilledButton(PrimaryButtonState.ICONPRESSED, text = "String", onClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FilledButtonPressed_preview() {
+    FilledButton(PrimaryButtonState.PRESSED, text = "String", onClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FilledButtonDisabled_icon_preview() {
+    FilledButton(PrimaryButtonState.ICONDISABLED, text = "String", onClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FilledButtonDisabled_preview() {
+    FilledButton(PrimaryButtonState.DISABLED, text = "String", onClick = {})
+}
+
+
