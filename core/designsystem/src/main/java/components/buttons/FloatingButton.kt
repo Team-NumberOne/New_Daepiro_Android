6fd0b5d883d7d4032a8cc094215708a4ee0ea2ac
seunghee17 <victoria9981@swu.ac.kr>
2024-03-19 21:26:52 +0900

    [Design] FloatingButton 수정

--- core/designsystem/src/main/java/components/buttons/FloatingButton.kt
+++ core/designsystem/src/main/java/components/buttons/FloatingButton.kt
@@ -6,16 +6,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -24,50 +21,56 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.numberone.daepiro.core.designsystem.R
 import com.numberone.daepiro.designsystem.theme.DaepiroTheme
-import model.DisableState
-import shadow.BtnPressedShadow
+import model.FloatingState
 import shadow.FloationShadow
 
 
 @Composable
-fun FloatingButton(state: DisableState, text:String, onClick: () -> Unit){
+fun FloatingButtonState(state: FloatingState, text:String, onClick: () -> Unit){
     when(state){
-        DisableState.DEFAULF -> DefaultFloatingButton(text = text, onClick = onClick)
-        DisableState.DISABLED -> DisableFloatingButton(text = text)
+        FloatingState.DEFAULF -> FloatingButton(text = text, onClick = onClick)
+        FloatingState.DISABLED -> FloatingButton(text = text , onClick = {  }, isDisable = true)
     }
 }
 
+
 @Composable
-fun DefaultFloatingButton(
+fun FloatingButton(
     modifier: Modifier = Modifier,
     text : String,
-    borderRadius : Dp = 20.dp,
-    onClick : () -> Unit
+    onClick : () -> Unit,
+    isDisable : Boolean = false
     ){
     val interactionSource = remember{ MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
-    val backgroundColor = if(isPressed) DaepiroTheme.colors.G_50 else DaepiroTheme.colors.On_Secondary
 
     Box(
-        modifier = Modifier.padding(borderRadius)
+        modifier = Modifier.padding(20.dp)
     ){
         Box(
             modifier = modifier
-                .FloationShadow(borderRadius = borderRadius)
+                .FloationShadow(borderRadius = 20.dp)
                 .background(
-                    color = backgroundColor,
+                    color = if (isPressed) DaepiroTheme.colors.G_50 else DaepiroTheme.colors.On_Secondary,
                     shape = RoundedCornerShape(20.dp)
                 )
-                .clickable(
-                    interactionSource = interactionSource,
-                    indication = null
-                ) {
-                    onClick()
-                },
+                .then(
+                    if (isDisable == true) {
+                        Modifier
+                    } else {
+                        Modifier.clickable(
+                            interactionSource = interactionSource,
+                            indication = null
+                        ) {
+                            onClick()
+                        }
+                    }
+                )
+
         ){
             Text(
                 text = text,
-                color = DaepiroTheme.colors.G_800,
+                color = if(isDisable == true) DaepiroTheme.colors.G_200 else DaepiroTheme.colors.G_800,
                 style = DaepiroTheme.typography.button,
                 modifier = Modifier
                     .padding( top = 9.dp, bottom = 9.dp, start = 24.dp)
@@ -83,45 +86,6 @@ fun DefaultFloatingButton(
     }
 }
 
-@Composable
-fun PressedFloatingButton(
-    modifier: Modifier = Modifier,
-    text : String,
-    borderRadius : Dp = 20.dp,
-    onClick : () -> Unit
-){
-    val interactionSource = remember{ MutableInteractionSource() }
-    Box(
-        modifier = Modifier.padding(borderRadius)
-    ){
-        Box(
-            modifier = modifier
-                .FloationShadow(borderRadius = borderRadius)
-                .background(color = DaepiroTheme.colors.G_50, shape = RoundedCornerShape(20.dp))
-                .clickable(
-                    interactionSource = interactionSource,
-                    indication = null
-                ) {
-                    onClick()
-                },
-        ){
-            Text(
-                text = text,
-                color = DaepiroTheme.colors.G_800,
-                style = DaepiroTheme.typography.button,
-                modifier = Modifier
-                    .padding( top = 9.dp, bottom = 9.dp, start = 24.dp)
-            )
-            Image(
-                modifier = Modifier
-                    .padding(start = 72.dp, top = 8.dp, bottom = 8.dp, end = 14.dp),
-                painter = painterResource(id = R.drawable.ic_plus),
-                colorFilter = ColorFilter.tint(DaepiroTheme.colors.G_900),
-                contentDescription = null
-            )
-        }
-    }
-}
 
 @Composable
 fun DisableFloatingButton(
@@ -157,15 +121,13 @@ fun DisableFloatingButton(
 }
 
 
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
-fun DefaultFloatingButtonPreview(){
-    FloatingButton(state = DisableState.DEFAULF , text = "TEXT" , onClick = {} )
+fun FloatingButtonPreview(){
+    Column(
+        modifier = Modifier
+    ){
+        FloatingButtonState(state = FloatingState.DEFAULF, text = "TEXT", onClick = {})
+        FloatingButtonState(state = FloatingState.DISABLED, text = "TEXT", onClick = {})
+    }
 }
-
-@Preview(showBackground = false)
-@Composable
-fun PressedDisableButtonPreview(){
-    FloatingButton(state = DisableState.DISABLED , text = "TEXT" ,onClick = {}  )
-}
-
--- core/designsystem/src/main/java/model/FloatingState.kt
+++ core/designsystem/src/main/java/model/FloatingState.kt
@@ -1,5 +1,5 @@
 package model
 
-enum class DisableState {
+enum class FloatingState {
     DEFAULF, DISABLED
 }
