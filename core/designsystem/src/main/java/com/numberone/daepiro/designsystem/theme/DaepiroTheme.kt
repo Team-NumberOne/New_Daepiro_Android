c99df5ab2baf2e9dc28c156d003bd7e34e26e079
seunghee17 <victoria9981@swu.ac.kr>
2024-03-20 19:28:55 +0900

    [Design] theme 전체적 수정

--- core/designsystem/src/main/java/com/numberone/daepiro/designsystem/theme/DaepiroTheme.kt
+++ core/designsystem/src/main/java/com/numberone/daepiro/designsystem/theme/DaepiroTheme.kt
@@ -3,14 +3,14 @@ package com.numberone.daepiro.designsystem.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 
-object DaepiroTheme {
-    val colors: DaepiroColors
-        @Composable
-        @ReadOnlyComposable
-        get() = LocalColors.current
-
-    val typography: DaepiroTypography
-        @Composable
-        @ReadOnlyComposable
-        get() = LocalTypography.current
-}
+//object DaepiroTheme {
+//    val colors: DaepiroColors
+//        @Composable
+//        @ReadOnlyComposable
+//        get() = LocalColors.current
+//
+//    val typography: DaepiroTypography
+//        @Composable
+//        @ReadOnlyComposable
+//        get() = LocalTypography.current
+//}
--- core/designsystem/src/main/java/com/numberone/daepiro/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/numberone/daepiro/designsystem/theme/Theme.kt
@@ -10,35 +10,66 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-fun colors() = DaepiroColors()
-fun fonts() = DaepiroTypography()
+//fun colors() = DaepiroColors()
+//fun fonts() = DaepiroTypography()
 
+//@Composable
+//fun DaepiroTheme(
+//    colors: DaepiroColors = colors(),
+//    typography: DaepiroTypography = fonts(),
+//    content: @Composable () -> Unit
+//) {
+//    val view = LocalView.current
+//
+//    SideEffect {
+//        if (!view.isInEditMode) {
+//            val window = (view.context as Activity).window
+//            window.statusBarColor = colors.White.toArgb()
+//        }
+//    }
+//
+//    CompositionLocalProvider(
+//        LocalColors provides colors,
+//        LocalTypography provides typography
+//    ) {
+//        content()
+//    }
+//
+//}
 @Composable
 fun DaepiroTheme(
-    colors: DaepiroColors = colors(),
-    typography: DaepiroTypography = fonts(),
-    content: @Composable () -> Unit
-) {
-    val view = LocalView.current
-
-    SideEffect {
-        if (!view.isInEditMode) {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colors.White.toArgb()
-        }
-    }
+    content: @Composable () -> Unit,
+){
+    val typographyInstance = DaepiroTypography()
 
     CompositionLocalProvider(
-        LocalColors provides colors,
-        LocalTypography provides typography
+        LocalColorScheme provides LightColorScheme,
+        LocalTypography provides typographyInstance
     ) {
-        content()
+        MaterialTheme(
+            colorScheme = lightColorScheme(
+                background = DaepiroTheme.colorScheme.background
+            ),
+            content = content
+        )
     }
+}
 
+object DaepiroTheme{
+    val typography: DaepiroTypography
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalTypography.current
+
+    val colorScheme: DaepiroColorScheme
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalColorScheme.current
 }
