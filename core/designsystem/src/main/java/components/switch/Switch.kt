8e459e7ea2e68e3a467dc90efb424ca2a70ca3c7
seunghee17 <victoria9981@swu.ac.kr>
2024-03-21 13:47:54 +0900

    [Design] switch select값 바뀌지 않는 오류 수정

--- core/designsystem/src/main/java/components/switch/Switch.kt
+++ core/designsystem/src/main/java/components/switch/Switch.kt
@@ -2,8 +2,9 @@ package components.switch
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -12,28 +13,20 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.numberone.daepiro.designsystem.theme.DaepiroTheme
-import components.buttons.DefaultFloatingButton
-import components.buttons.DisableFloatingButton
-import model.DisableState
-//수정 가능성
-enum class SwitchState{
-    DEFAULT, PRESSED
-}
-
-@Composable
-fun Switch(state: SwitchState, onClick: () -> Unit){
-    when(state){
-        SwitchState.DEFAULT -> Switch(selected = false, onClick = onClick)
-        SwitchState.PRESSED -> Switch(selected = true, onClick = onClick)
-
-    }
-}
+import com.numberone.daepiro.designsystem.theme.G_100
+import com.numberone.daepiro.designsystem.theme.G_200
+import com.numberone.daepiro.designsystem.theme.O_600
+import com.numberone.daepiro.designsystem.theme.Primary
+import com.numberone.daepiro.designsystem.theme.White
 
 @Composable
 fun ToggleCircle(
@@ -42,32 +35,41 @@ fun ToggleCircle(
     Card(
         shape = CircleShape, modifier = modifier.size(14.dp)
     ){
-        Box(modifier = Modifier.background(Color.White) )
+        Box(modifier = Modifier.background(White) )
     }
 }
 
 
 @Composable
 fun Switch(
-    selected: Boolean,
+    selected: MutableState<Boolean>,
     modifier : Modifier = Modifier,
     onClick : () -> Unit
 ){
+    val interactionSource = remember{ MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
     Card(
         modifier = modifier
             .width(38.dp)
             .clickable {
-                !selected
                 onClick()
             },
         shape = RoundedCornerShape(99.dp)
     ){
         Box(
             modifier = Modifier.background(
-                if(selected) DaepiroTheme.colors.Primary else DaepiroTheme.colors.G_200
+                if(isPressed){
+                    if(selected.value == false){
+                        O_600
+                    }else{
+                        G_100
+                    }
+                }else{
+                    if(selected.value == true) Primary else G_200
+                }
             )
                 .fillMaxWidth(),
-            contentAlignment = if (selected) Alignment.CenterEnd else Alignment.CenterStart
+            contentAlignment = if (selected.value == true) Alignment.CenterEnd else Alignment.CenterStart
         ){
             ToggleCircle(modifier = Modifier.padding(4.dp))
         }
@@ -77,5 +79,5 @@ fun Switch(
 @Preview(showBackground = true)
 @Composable
 fun SwitchPreview(){
-    Switch(selected = true, onClick = {})
+    //Switch(selected = true, onClick = {})
 }
