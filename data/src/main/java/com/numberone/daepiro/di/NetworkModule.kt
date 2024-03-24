ad4fc54d54ecd139fe1a684674902a0ef387039c
Ojongseok <whdtjr6889@naver.com>
2024-03-24 22:54:06 +0900

    build: API 주소 BuildConfig로 가리기

---
+++ data/src/main/java/com/numberone/daepiro/di/NetworkModule.kt
@@ -0,0 +1,56 @@
+package com.numberone.daepiro.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.numberone.daepiro.data.BuildConfig
+import com.numberone.daepiro.network.NetworkService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL = BuildConfig.BASE_URL
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideService(retrofit: Retrofit): NetworkService {
+        return retrofit.create(NetworkService::class.java)
+    }
+}
