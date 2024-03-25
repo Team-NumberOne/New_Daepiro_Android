f2e769349ba7e9b76238008e7fe968bf0207efd7
Ojongseok <whdtjr6889@naver.com>
2024-03-25 17:37:30 +0900

    build: 카카오로그인 관련 sdk 및 의존성 추가

--- app/src/main/java/com/numberone/daepiro/DaepiroApplication.kt
+++ app/src/main/java/com/numberone/daepiro/DaepiroApplication.kt
@@ -1,9 +1,15 @@
 package com.numberone.daepiro
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class DaepiroApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
 
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
 }
